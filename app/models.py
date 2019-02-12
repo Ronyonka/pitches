6157ec1a52d850fbace53273b26ace9e03c51109
Ronyonka <rononyonka@gmail.com>
2019-02-12 08:17:26 +0300

    added password hashing

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,7 +9,13 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)   
+        
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password) 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
