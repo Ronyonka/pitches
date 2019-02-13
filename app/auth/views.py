6d8787d82d5acc4974fe2a67c3c224b2f442f55e
Ronyonka <rononyonka@gmail.com>
2019-02-13 09:29:58 +0300

    added auth folder

---
+++ app/auth/views.py
@@ -0,0 +1,45 @@
+from flask import render_template,redirect,url_for,flash,request
+from . import auth
+from ..models import User
+from .. import db
+from flask_login import current_user, login_user, logout_user, login_required
+from .forms import LoginForm,RegistrationForm,EditProfileForm
+from werkzeug.urls import url_parse
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('auth/login.html', title='Sign In', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('auth/register.html', title='Register', form=form)
